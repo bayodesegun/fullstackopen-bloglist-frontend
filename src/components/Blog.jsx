664b47ca93ffd8e9017f546ba28e9b5b92e414fc
Bayode Aderinola <bayodesegun@gmail.com>
2023-11-27 01:18:05 +0100

    Exercise 5.7 Blog list frontend, step7: add logic for showing/hiding blog details

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      { !showDetails ?
+        (<div>
+          {blog.title} {blog.author} <button onClick={() => setShowDetails(true)}>view</button>
+        </div>
+        )
+        :
+        (<div>
+          <div>{blog.title} {blog.author} <button onClick={() => setShowDetails(false)}>hide</button></div>
+          <div>{blog.url}</div>
+          <div>{blog.likes} likes <button>like</button> </div>
+          <div>{blog.user ? blog.user.name : ''}</div>
+        </div>
+        )
+      }
+    </div>
+  )
+}
 
 export default Blog
